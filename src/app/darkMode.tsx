1ba45e3e49c5ee178e865bb2c7f1c9e09ef7be5f
jjou33 <zanda33@naver.com>
2023-11-03 09:15:09 +0900

    [#develop] test

--- src/app/darkMode.tsx
+++ src/app/darkMode.tsx
@@ -9,7 +9,7 @@ const DarkMode = () => {
   const cookie = ''
 
   useEffect(() => {
-    console.log('pus1htes33344')
+    console.log('pus1htes333')
     if (cookie === '') {
       document.cookie = 'mode=light; max-age=' + 3600 * 24 * 400
     }
