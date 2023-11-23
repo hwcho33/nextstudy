f8a8192c96aa37df78507cdba034ac47c28bcfbb
jjou33 <zanda33@naver.com>
2023-11-23 22:10:39 +0900

    [#develop] test

--- src/utils/database.ts
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from 'mongodb'
-
-let connectDB: Promise<MongoClient>
-interface OverridingGlobalType extends Global {
-  _mongo: Promise<MongoClient>
-}
-
-export { connectDB }
